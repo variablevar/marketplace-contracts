1f3761ce924e98d8d004c2f8ad77457d6babc72d
Dhruvin Beladiya <dhruvinbeladiya01@gmail.com>
2024-03-11 18:07:41 +0530

    USER SCHEMA EXPORTED

---
+++ lib/models/index.ts
@@ -0,0 +1,39 @@
+import AcceptedNFTModel from "./accepted-nft";
+import BoughtNFTModel from "./bought-nft";
+import CanceledOfferedNFTModel from "./canceled-offered-nft";
+import CreatedAuctionModel from "./created-auction";
+import ListedNFTModel from "./listed-nft";
+import OfferedNFTModel from "./offered-nft";
+import PlacedBidModel from "./placed-bid";
+import ResultedAuctionModel from "./resulted-auction";
+import NFTCollectionModel from "./nft-collection";
+import TokenModel from "./token";
+import {UserModel} from "./user";
+import {AvatarModel} from "./avatar";
+import { AuthorRanksModel } from "./author-sale";
+import { BidModel } from "./bid";
+import { NftModel, PreviewImageClassModel } from "./nft";
+import HotCollectionModel, { HotCollectionBannerModel, PurpleFormatsModel } from "./hot-collection";
+
+export {
+    AcceptedNFTModel,
+    BoughtNFTModel,
+    CanceledOfferedNFTModel,
+    CreatedAuctionModel,
+    ListedNFTModel,
+    OfferedNFTModel,
+    PlacedBidModel,
+    ResultedAuctionModel,
+    NFTCollectionModel,
+    TokenModel,
+    UserModel,
+    AvatarModel,
+    AuthorRanksModel,
+    BidModel,
+    NftModel,
+    PreviewImageClassModel,
+    HotCollectionModel,
+    HotCollectionBannerModel,
+    PurpleFormatsModel
+  };
+  
