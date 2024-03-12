f082ebf5262e8fc96aa42c930871e01ecce33331
variable <variable@variable.variable>
2024-03-12 12:24:28 +0530

    USER SCHEMA BCRYPT ADDED

--- lib/models/user.ts
+++ lib/models/user.ts
@@ -1,18 +1,18 @@
-import mongoose, { Document, Schema } from 'mongoose';
-import { AuthorSaleSchema, IAuthorSale } from './author-sale';
-import { AvatarSchema, IAvatar } from './avatar';
-import { BidSchema, IBid } from './bid';
-import { INft, IPreviewImageClass, NftSchema, PreviewImageClassSchema } from './nft';
-import { HotCollectionSchema, IHotCollection } from './hot-collection';
-
+import { compare, hash } from "bcrypt";
+import mongoose, { Document, Schema } from "mongoose";
+import { IAuthorSale } from "./author-sale";
+import { IAvatar } from "./avatar";
+import { IBid } from "./bid";
+import { IHotCollection } from "./hot-collection";
+import { INft, IPreviewImageClass } from "./nft";
 
 export interface IUser extends Document {
   username: string;
-  password:string;
+  password: string;
   email: string;
   wallet: string;
   followers?: IUser[];
-  bid?: IBid ;
+  bid?: IBid;
   author_sale?: IAuthorSale;
   about?: string;
   published_at?: Date;
@@ -22,36 +22,54 @@ export interface IUser extends Document {
   banner?: IPreviewImageClass;
   nfts?: INft[];
   hot_collections?: IHotCollection[];
+  comparePassword: (candidatePassword: string) => Promise<boolean>;
 }
 
-  export const UserSchema: Schema<IUser> = new Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true },
-    wallet: { type: String, default : '' },
-    followers: {type:[{ type: Schema.Types.ObjectId, ref: 'User' }],default:[]},
-    bid: { type: Schema.Types.ObjectId, ref: 'Bid' },
-    author_sale: { type: Schema.Types.ObjectId, ref: 'AuthorSale' } ,
-    about: { type: String, default: ''},
-    published_at: {
-      type: Date,
-      default: Date.now // Set default value to current date and time
-    },
-    created_at: {
-      type: Date,
-      default: Date.now // Set default value to current date and time
-    },
-    updated_at: {
-      type: Date,
-      default: Date.now // Set default value to current date and time
-    },
-    avatar:  { type: Schema.Types.ObjectId, ref: 'Avatar' },
-    banner:   { type: Schema.Types.ObjectId, ref: 'PreviewImageClass' } ,
-    nfts: {type:[{ type: Schema.Types.ObjectId, ref: 'Nft' }],default:[]},
-    hot_collections: {type:[{ type: Schema.Types.ObjectId, ref: 'HotCollection' }], default: []}
-  });
+export const UserSchema: Schema<IUser> = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  wallet: { type: String, default: "" },
+  followers: {
+    type: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    default: [],
+  },
+  bid: { type: Schema.Types.ObjectId, ref: "Bid" },
+  author_sale: { type: Schema.Types.ObjectId, ref: "AuthorSale" },
+  about: { type: String, default: "" },
+  published_at: {
+    type: Date,
+    default: Date.now, // Set default value to current date and time
+  },
+  created_at: {
+    type: Date,
+    default: Date.now, // Set default value to current date and time
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now, // Set default value to current date and time
+  },
+  avatar: { type: Schema.Types.ObjectId, ref: "Avatar" },
+  banner: { type: Schema.Types.ObjectId, ref: "PreviewImageClass" },
+  nfts: { type: [{ type: Schema.Types.ObjectId, ref: "Nft" }], default: [] },
+  hot_collections: {
+    type: [{ type: Schema.Types.ObjectId, ref: "HotCollection" }],
+    default: [],
+  },
+});
 
+UserSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
 
+  const hashed = await hash(user.password, 10);
+  user.password = hashed;
+  next();
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword: string) {
+  return compare(candidatePassword, this.password);
+};
 
 // Create and export the mongoose model
-export const UserModel = mongoose.model<IUser>('User', UserSchema);
+export const UserModel = mongoose.model<IUser>("User", UserSchema);
